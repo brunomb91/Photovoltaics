228761a8a8c981b808ebfb9735884a7a9a430598
bruno <bruno.mb.91@gmail.com>
2018-11-07 16:47:15 -0300

    Changing Arduino data to be a JSON

--- current_and_voltage_measuring/current_and_voltage_measuring.ino
+++ current_and_voltage_measuring/current_and_voltage_measuring.ino
@@ -8,7 +8,7 @@ void setup()
 {  
   Serial.begin(9600);
   
-  emon1.current(1, 6);             // Current: input pin, calibration.
+  emon1.current(1, 6.75);             // Current: input pin, calibration.
   emon1.voltage(5, 610, 1.7);  // Voltage: input pin, calibration, phase_shift
 
 }
@@ -18,11 +18,18 @@ void loop()
   double Irms = emon1.calcIrms(1480);  // Calculate Irms only
   emon1.calcVI(20,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
 
-  Serial.print("Irms: ");
-  Serial.println(emon1.Irms);
-  Serial.print("Vrms: ");
-  Serial.println(emon1.Vrms);
-  Serial.print("Power: ");
-  Serial.println(emon1.Irms*emon1.Vrms);         // Apparent power
+  //Serial.print("{"); 
+  Serial.print("Irms");
+  Serial.print(":");
+  Serial.print(Irms);
+  Serial.print(",");
+  Serial.print("Vrms");
+  Serial.print(":");
+  Serial.print(emon1.Vrms);
+  Serial.print(",");
+  Serial.print("Power");
+  Serial.print(":");
+  Serial.print(Irms*emon1.Vrms);         // Apparent power
+  //Serial.print("}"); 
   delay(2000);
 }
--- python_src/serial_access.py
+++ python_src/serial_access.py
@@ -3,20 +3,37 @@
 # From https://www.raspberrypi.org/forums/viewtopic.php?t=111882#p767117
 
 import serial, string, time
+import json
 
 output = " "
 ser = serial.Serial('/dev/ttyACM0', 9600, 8, 'N', 1, timeout=1)
 
-while True:
-    try:
-        while output != "":
-            output = ser.readline()
-            print output
-            output = " "
-    except KeyboardInterrupt:
-        print "Interrupted by the user."
-        break
-    except serial.serialutil.SerialException:
-        print "Serial used by another user."
-        break
+def convert_to_dict(name):
+   list1 = name.split(",")
+   
+   list2 = []
+    
+   for i in list1:
+      list2.append(tuple(i.split(":")))
+   tuple1 = tuple(list2)   
+    
+   y = dict(tuple1)
+   return json.dumps(y, indent=4)
+
+try:
+    while True:
+        output = ser.readline()
+        if output == "" or len(output) >= 33:
+            continue
+        # print output
+        else:
+            y = convert_to_dict(output)
+            print y
+        # output = " "
+except KeyboardInterrupt:
+    print "Interrupted by the user."
+    # break
+except serial.serialutil.SerialException:
+    print "Serial used by another user."
+    # break
          
